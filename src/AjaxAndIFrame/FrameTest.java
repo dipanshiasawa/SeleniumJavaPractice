4229139de039d4580c3178637e196fb2bbfbb330
Dipanshi Asawa <dipanshiasawa1402@gmail.com>
2022-07-13 16:01:43 +0530

    Drag and Drop Actions

--- src/AjaxAndIFrame/FrameTest.java
+++ src/AjaxAndIFrame/FrameTest.java
@@ -3,6 +3,7 @@ package AjaxAndIFrame;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class FrameTest {
 
@@ -15,7 +16,8 @@ public class FrameTest {
 		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe.demo-frame")));
 		
 		driver.findElement(By.id("draggable")).click();
-		
+		Actions a = new Actions(driver);
+		a.dragAndDrop(driver.findElement(By.id("draggable")), driver.findElement(By.id("droppable"))).build().perform();
 
 	}
 
