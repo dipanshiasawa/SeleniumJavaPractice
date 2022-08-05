2bdd6caa01036b304ada74b0e1fdf5ef02d6223c
Dipanshi Asawa <dipanshiasawa1402@gmail.com>
2022-08-06 01:31:33 +0530

    Take screenshots

--- src/Miscellaneous/Miscellaneous.java
+++ src/Miscellaneous/Miscellaneous.java
@@ -1,18 +1,37 @@
 package Miscellaneous;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Miscellaneous {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		System.setProperty("webdriver.chrome.driver", "D:\\Browser_Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().deleteCookieNamed("asdf");
+		//if we want to check the session validation and check if it is 
+		//navigating to login page or not, we will delete the cookie
+		
+		driver.manage().deleteCookieNamed("SessionKey");
+		//Now click on anything and check if it is navigation to login page or not.
+		
 		
 		driver.get("https://google.com");
+		
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("D:\\Synapse\\screenshot.png"));
+		
+		
 
 	}
 
