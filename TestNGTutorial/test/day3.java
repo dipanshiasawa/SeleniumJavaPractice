396aa378eec64064ef3864e137eaf8c587fb7831
Dipanshi Asawa <dipanshiasawa1402@gmail.com>
2022-08-18 18:12:55 +0530

    Regular Expression in XML

--- TestNGTutorial/test/day3.java
+++ TestNGTutorial/test/day3.java
@@ -17,6 +17,18 @@ public class day3 {
 	}
 	
 	@Test
+	private void MobilesigninCarLoan() {
+		//Appium code
+		System.out.println("mobile signin car");
+	}
+	
+	@Test
+	private void MobilesignoutCarLoan() {
+		//Appium code
+		System.out.println("mobile signout car");
+	}
+	
+	@Test
 	private void LoginAPICarLoan() {
 		//API code
 		System.out.println("api login car");
