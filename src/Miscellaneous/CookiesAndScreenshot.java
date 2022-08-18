8cdf76f05da8abd3557852903bee2044fbd32cba
Dipanshi Asawa <dipanshiasawa1402@gmail.com>
2022-08-18 16:37:38 +0530

    Screenshot of window and elements. Relative Locators(Above, below, left, right). Switching browser tabs.

--- src/Miscellaneous/CookiesAndScreenshot.java
+++ src/Miscellaneous/CookiesAndScreenshot.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Miscellaneous {
+public class CookiesAndScreenshot {
 
 	public static void main(String[] args) throws IOException {
 
--- src/Selenium4/NewWindow.java
+++ src/Selenium4/NewWindow.java
@@ -1,16 +1,21 @@
 package Selenium4;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.setProperty("webdriver.chrome.driver", "D:\\Browser_Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
@@ -27,8 +32,12 @@ public class NewWindow {
 		String courseName = driver.findElements(By.cssSelector("[href*='https://courses.rahulshettyacademy.com/p/']")).get(1).getText();
 		
 		driver.switchTo().window(parentId);
-		driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseName);
+		WebElement name = driver.findElement(By.cssSelector("[name='name']"));
+		name.sendKeys(courseName);
+		//Get a screenshot of name webElement
 		
+		File file = name.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("logo.png"));
 
 	}
 
